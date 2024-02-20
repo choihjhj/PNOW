3addd7520b46a745edffecf81c356114ec90138b
choihyeji <hyejichoi14@gmail.com>
2024-02-21 01:20:31 +0900

    주소소분류 선택시 districtId 세션에 저장하는 @GetMapping("/district/{districtId}") 메서드 완료

--- src/main/java/com/pnow/controller/DistrictController.java
+++ src/main/java/com/pnow/controller/DistrictController.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
@@ -18,15 +17,20 @@ public class DistrictController {
     private final DistrictService districtService;
     private final HttpSession httpSession;
 
-    @GetMapping("/district/{cityId}")
-
+    @GetMapping("/district/list/{cityId}")
     public List<DistrictDTO> districtListRead(@PathVariable("cityId") Long cityId){
-        log.info("district 메소드 진입. cityId: {}", cityId);
+        log.info("districtDTO 목록 조회 메소드 진입. cityId: {}", cityId);
 
-        //공용으로 쓸 데이터(cityId) 세션에 저장
+        //선택된 데이터(cityId) 세션에 저장
         httpSession.setAttribute("cityId",cityId);
 
         // DistrictDTO 반환
         return districtService.findDistrictsWithCityId(cityId);
+    }
+    @GetMapping("/district/{districtId}")
+    public void districtIdSessionSet(@PathVariable("districtId") Long districtId){
+        log.info("districtId 세션에 저장 메소드 진입. districtId: {}", districtId);
+        //선택된 데이터(districtId) 세션에 저장
+        httpSession.setAttribute("districtId",districtId);
     }
 }
