ff59e82e51c2c27e6c023fffeca3a20ecac78918
choihyeji <hyejichoi14@gmail.com>
2024-02-22 17:14:04 +0900

    주석처리 추가

--- src/main/java/com/pnow/repository/DistrictRepository.java
+++ src/main/java/com/pnow/repository/DistrictRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 @Repository
 public interface DistrictRepository extends JpaRepository<District, Long> {
     // 도시의 시군구 리스트를 DistrictDTO로 가져오는 쿼리
-    @Query("SELECT new com.pnow.dto.DistrictDTO(d.id, d.districtName) FROM District d WHERE d.city.id = :cityId")
+    @Query("SELECT new com.pnow.dto.DistrictDTO(d.id, d.districtName) FROM District d WHERE d.city.id = :cityId") //DTO에 바로 받아 넣기
     List<DistrictDTO> findDistrictDTOsByCityId(@Param("cityId") Long cityId);
 }
