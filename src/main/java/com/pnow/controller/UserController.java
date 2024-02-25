1d5a639590ee0b691711d9d814da606b39985033
choihyeji <hyejichoi14@gmail.com>
2024-02-26 03:39:21 +0900

    내정보 myinfo.html 틀 만들었음, 수정탈퇴기능구현하기

--- src/main/java/com/pnow/controller/UserController.java
+++ src/main/java/com/pnow/controller/UserController.java
@@ -1,9 +1,35 @@
 package com.pnow.controller;
 
+import com.pnow.config.auth.LoginUser;
+import com.pnow.config.auth.dto.SessionUserDTO;
+import com.pnow.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/users")
+@RequiredArgsConstructor
 @Controller
-
+@Slf4j
 public class UserController {
+    private final UserService userService;
+
+    /*
+     * 내정보로 페이지 이동 myinfo.html
+     * - 유저정보 model에 저장
+     * GET /users
+     * return "/users/myinfo"
+     * */
+    @GetMapping
+    public String userMyinfo(@LoginUser SessionUserDTO user, Model model){
+        log.info("내정보 페이지 진입 user={}",user.getName());
+        if(user != null){
+            model.addAttribute("user", userService.findUser(user));
+        }
+        return "users/myinfo";
+    }
 
 }
--- src/main/java/com/pnow/service/UserService.java
+++ src/main/java/com/pnow/service/UserService.java
@@ -1,11 +1,23 @@
 package com.pnow.service;
 
+import com.pnow.config.auth.dto.SessionUserDTO;
+import com.pnow.domain.user.User;
 import com.pnow.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public User findUser(SessionUserDTO user){
+        return userRepository.findById(user.getId())
+                .orElseThrow(() -> new EntityNotFoundException("User not found" + user.getId()));
+
+    }
 }
