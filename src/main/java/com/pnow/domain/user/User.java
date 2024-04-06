e312920b92c5dd6b50b896ebafe18dfbb26fc997
choihyeji <hyejichoi14@gmail.com>
2024-04-06 17:34:06 +0900

    주석처리지움

--- src/main/java/com/pnow/domain/user/User.java
+++ src/main/java/com/pnow/domain/user/User.java
@@ -42,9 +42,6 @@ public class User extends BaseTimeEntity {
     @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
     private List<Reservation> reservationList;
 
-//    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
-//    private List<Review> reviewList;
-
     @Builder
     public User(String name, String email, String picture, Role role) {
         this.name = name;
@@ -65,55 +62,3 @@ public class User extends BaseTimeEntity {
     }
 
 }
-//package com.pnow.domain.user;
-//
-//import com.pnow.domain.BaseTimeEntity;
-//import lombok.Builder;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//
-//import javax.persistence.*;
-//
-//@Getter
-//@NoArgsConstructor
-//@Table(name = "users") //Posts와 구분하고자 넣음, 근데 user는 예약어라서 쓰면 안되니까 users라고 테이블명 셋팅
-//@Entity
-//public class User extends BaseTimeEntity {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @Column(nullable = false)
-//    private String name;
-//
-//    @Column(nullable = false)
-//    private String email;
-//
-//    @Column
-//    private String picture;
-//
-//    @Enumerated(EnumType.STRING)
-//    @Column(nullable = false)
-//    private Role role;
-//
-//    @Builder
-//    public User(String name, String email, String picture, Role role) {
-//        this.name = name;
-//        this.email = email;
-//        this.picture = picture;
-//        this.role = role;
-//    }
-//
-//    public User update(String name, String picture) {
-//        this.name = name;
-//        this.picture = picture;
-//
-//        return this;
-//    }
-//
-//    public String getRoleKey() {
-//        return this.role.getKey();
-//    }
-//
-//}
