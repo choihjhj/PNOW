54c2a1cd72df2ef98491b3189fa09dc83eb4c468
choihyeji <hyejichoi14@gmail.com>
2024-04-13 13:50:38 +0900

    validation 유효성 검사 추가

---
+++ src/main/java/com/pnow/service/ValidationService.java
@@ -0,0 +1,13 @@
+package com.pnow.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
+@Service
+@Validated
+public class ValidationService {
+    public  <T> void checkValid(@Valid T validationTarget) {
+        // do nothing
+    }
+}
