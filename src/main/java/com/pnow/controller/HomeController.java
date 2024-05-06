a7b6d174cad533bd1ea4c5b669462b8e5e5a5b17
choihyeji <hyejichoi14@gmail.com>
2024-05-06 18:13:35 +0900

    임시로 주석처리

--- src/main/java/com/pnow/controller/HomeController.java
+++ src/main/java/com/pnow/controller/HomeController.java
@@ -32,8 +32,8 @@ public class HomeController {
         log.info("root 메소드 진입 user = {}", user);
 
         //예약테이블의 오늘 날짜현재시간 지난 거 reservationStatus = ReservationStatus.COMPLETE 처리
-        reservationService.updateUserReservationStatus(); /*관리자 페이지 없어서 임의로 자동처리 */
-        log.info("--예약테이블셋팅 완료--");
+//        reservationService.updateUserReservationStatus(); /*관리자 페이지 없어서 임의로 자동처리 */
+//        log.info("--예약테이블셋팅 완료--");
 
         if(user != null){
 
